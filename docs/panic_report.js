4bf8a3e9f1c340b31e0859fe73dcbcf1e783ac8d
Jocelyn Falempe <jfalempe@redhat.com>
2024-12-04 18:20:57 +0100

    Add Fido2 encoding support.
    Main difference, is that it uses a 7 bytes chunk converted to 17
    decimal digits, instead of 13 bits chunk converted to 4 decimal
    digits.
    
    https://github.com/kdj0c/panic_report/issues/2
    for backward compatibility, use z=xxxxx for the new encoding.
    
    Signed-off-by: Jocelyn Falempe <jfalempe@redhat.com>

--- docs/panic_report.js
+++ docs/panic_report.js
@@ -5,6 +5,38 @@ const urlParams = new URLSearchParams(queryString);
 
 var main_div = document.getElementById('main');
 
+/*
+ * Encoding, same as Fido2 specification.
+ * v6.14+
+ */
+function numbers_to_data2(numbers) {
+        /* 17 decimal digits are converted to 7 bytes */
+        let main_len = Math.floor(numbers.length / 17) * 7;
+        /* and the remainings bytes */
+        let rem_len = Math.floor((numbers.length % 17) + 2 / 3);
+
+        let data = new Uint8Array(main_len + rem_len);
+        let offset = 0;
+
+        for (var i = 0; i < numbers.length; i += 17) {
+                let chunk = numbers.substring(i, i + 17);
+                let num = BigInt(chunk);
+                let num_bytes = 7;
+                if (chunk.len < 17) {
+                        num_bytes = rem_len;
+                }
+                for (var j = 0; j < num_bytes; j++) {
+                        data[offset] = Number(num % 256n);
+                        num = num / 256n;
+                        offset++;
+                }
+        }
+        return data;
+}
+
+/*
+ * Legacy encoding, found in v6.10 to v6.13
+ */
 function numbers_to_data(numbers) {
         const num_chars_to_bits = [0, 4, 7, 10, 13];
         const length_in_bits = Math.floor(numbers.length / 4) * 13 + num_chars_to_bits[numbers.length % 4];
@@ -58,15 +90,25 @@ function add_info(name, param) {
         }
 }
 
+function add_data(data) {
+	uncompressed = pako.inflate(new Uint8Array(data));
+	text = String.fromCharCode.apply(null, uncompressed);
+
+	kmsg = document.createElement("pre");
+	kmsg.appendChild(document.createTextNode(text));
+	main_div.appendChild(kmsg);
+}
+
 add_info("Arch", "a");
 add_info("Version", "v");
 add_info("Distribution", "d");
 
-const numbers = urlParams.get("zl");
-data = numbers_to_data(numbers);
-uncompressed = pako.inflate(new Uint8Array(data));
-text = String.fromCharCode.apply(null, uncompressed);
-
-kmsg = document.createElement("pre");
-kmsg.appendChild(document.createTextNode(text));
-main_div.appendChild(kmsg);
+const numbers = urlParams.get("z");
+const legacy_numbers = urlParams.get("zl")
+if (numbers) {
+	data = numbers_to_data2(numbers);
+	add_data(data);
+} else if (legacy_numbers) {
+	data = numbers_to_data(legacy_numbers);
+	add_data(data);
+}
